0ce8af0b8e006a338b6cf79629ba1ed23dbbd15d
Andrew McGregor <me@andrewmcgregor.me>
2016-06-16 23:16:41 -0400

    1

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int sum_of_natural_numbers(int upto)
+{
+  int sum = 0;
+
+  for (int i = 0; i < upto; i++)
+    if (i % 3 == 0 || i % 5 == 0)
+      sum += i;
+
+  return sum;
+}
+
+int main(int argc, char** argv)
+{
+  printf("%i\n", sum_of_natural_numbers(1000));
+
+  return 1;
+}
+
