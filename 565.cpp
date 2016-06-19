a6df244bf96987b32981b99575a3d773d760e4a3
Andrew McGregor <me@andrewmcgregor.me>
2016-06-19 07:46:02 -0400

    565 wip

--- 565.cpp
+++ 565.cpp
@@ -23,67 +23,35 @@ int64_t sum_of_divisors_upto(int64_t upto, int64_t divisor)
 
   std::vector<int64_t> divisible;
 
-  bool check = true;
-
   // find lowest divisible sum of divisors
   for (int64_t i = 1; i < upto; i++) {
-    /*
-    printf("checking: %" PRId64 "\n", i);
-
-    printf("divisible: \n");
-    for (int x = 0; x < divisible.size(); x++)
-      printf("%" PRId64 " ", divisible[x]);
-    printf("\n");
-    */
-
-    if (divisible.size() != 0) {
-      for (int64_t j = 0; j < divisible.size(); j++) {
-        if (i % divisible[j] == 0) {
-          check = true;
-          break;
-        }
-      }
-    } else {
-      check = true;
-    }
-
     current_divisors = sum_of_divisors(i);
 
     if (current_divisors % divisor == 0) {
       sum += current_divisors;
       divisible.push_back(i);
-      divisible.push_back(i + 1);
-      printf("%" PRId64 "\n", i);
+      break;
     }
-
-    check = false;
   }
 
-  /*
-  printf("---------\n");
+  //printf("found lowest divisor: %" PRId64 "\n", divisible[0]);
 
-  int64_t last_divisible_size = 0;
-
-  while (last_divisible_size != divisible.size()) {
-    last_divisible_size = divisible.size();
-
-    for (int64_t i = 0; i < divisible.size(); i++) {
-      int64_t to_check = divisible[i] + divisible[j];
-      printf("%" PRId64 "\n", to_check);
-
-      if (to_check < upto &&
-          std::find(added.begin(), added.end(), to_check) == added.end()) {
-        current_divisors = sum_of_divisors(to_check);
+  for (int64_t i = divisible[0] * 2; i < upto; i++) {
+    for (int64_t j = 0; j < divisible.size(); j++) {
+      if (i % divisible[j] == 0 || (i - 1) % divisible[j] == 0) {
+      //if (i % divisible[j] == 0) {
+      //if (true) {
+        current_divisors = sum_of_divisors(i);
 
         if (current_divisors % divisor == 0) {
-          divisible.push_back(to_check);
           sum += current_divisors;
-          added.push_back(to_check);
+          //printf("%" PRId64 "\n", i);
+          divisible.push_back(i);
         }
+        break;
       }
     }
   }
-  */
 
   return sum;
 }
@@ -91,13 +59,13 @@ int64_t sum_of_divisors_upto(int64_t upto, int64_t divisor)
 int main(int argc, char** argv)
 {
   //printf("test case 0: 49 == %" PRId64 "\n", sum_of_divisors_upto(20, 7));
-  printf("test case 0: 49 == %" PRId64 "\n", sum_of_divisors_upto(30, 7));
-  //printf("test case x: 150850429 == %" PRId64 "\n", sum_of_divisors_upto(pow(10, 6), 2017));
+  //printf("test case 0: 189 == %" PRId64 "\n", sum_of_divisors_upto(30, 7));
+  //printf("test case 0: 1001 == %" PRId64 "\n", sum_of_divisors_upto(70, 7));
+
   //printf("test case 1: 150850429 == %" PRId64 "\n", sum_of_divisors_upto(pow(10, 6), 2017));
+  //printf("test case 2: 249652238344557 == %" PRId64 "\n", sum_of_divisors_upto(pow(10, 9), 2017));
 
-  //printf("test case 2: 249652238344557 == %" PRId64 "\n", sum_of_divisors(pow(10, 9), 2017));
-
-  //printf("%i\n", sum_of_divisors(pow(10, 11), 2017));
+  printf("%" PRId64 "\n", sum_of_divisors_upto(pow(10, 11), 2017));
 
   return 0;
 }
